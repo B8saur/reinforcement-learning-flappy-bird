a33d7b16821ff4477dfe0b1b62dc2763c3193498
Jacek Markiewicz <jacekmarkiewicz6@onet.pl>
2025-06-16 11:51:01 +0200

    NN kind of works

--- nn.py
+++ nn.py
@@ -3,18 +3,14 @@ from rlfb_config import *
 from engine import *
 import random
 from collections import deque
-from copy import copy
+from copy import copy, deepcopy
 from tqdm import tqdm 
 
 def leaky_relu(x):
-    return np.where(x>0, x, 0.01*x)
+    return np.where(x>0, x, 0.1*x)
 
 def leaky_relu_dx(x):
-    return np.where(x>0, 1.0, 0.01)
-
-# def sigmoid(x):
-#     x = np.clip(x, -500, 500)
-#     return 1 / (1 + np.exp(-x))
+    return np.where(x>0, 1.0, 0.1)
 
 def get_useful_state(game_state):
     game_state = copy(game_state)
@@ -24,18 +20,27 @@ def get_useful_state(game_state):
 
     next_pipe_mid = (next_pipe_low + next_pipe_high)/2
 
+    # Clamp values to avoid weird scenarios
+
+    bird_height = np.clip(bird_height, 0, 1)
+    bird_speed_vertical = np.clip(bird_speed_vertical, -1, 1)
+    next_pipe_distance = np.clip(next_pipe_distance, -1, 1) # This is not true, BUT only for the first pipe
+    next_pipe_mid = np.clip(next_pipe_mid, 0, 1)
+
     q_state = bird_height, bird_speed_vertical, next_pipe_distance, next_pipe_mid
     return q_state, status
 
 class Model_NN:
     def __init__(self):
         self.lr = 0.0001
-        self.layer_sizes = [4, 16, 8, 2]
+        self.layer_sizes = [4, 16, 16, 2]
         self.replay = deque(maxlen=50000)
         self.batch_size = 64
 
         self.weights = []
         self.biases = []
+
+        self.target_model = deepcopy(self)
 
         for i in range(len(self.layer_sizes)-1):
             num_in, num_out = self.layer_sizes[i], self.layer_sizes[i+1]
@@ -44,6 +49,10 @@ class Model_NN:
             b = np.zeros((num_out, 1))
             self.weights.append(w)
             self.biases.append(b)
+
+    def update_target_model(self):
+        self.target_model.weights = [w.copy() for w in self.weights]
+        self.target_model.biases = [b.copy() for b in self.biases]
 
     def forward_pass(self, input):
         a = np.array(input, dtype=np.float32).reshape(-1, 1)
@@ -63,16 +72,13 @@ class Model_NN:
 
         return activations[-1], activations, z_cache
     
-    def backward_pass(self, activations, z_cache, out_target, action_index):
+    def backward_pass(self, activations, z_cache, target_vec, action_index):
         grad_w = [np.zeros_like(w) for w in self.weights]
         grad_b = [np.zeros_like(b) for b in self.biases]
 
-        out = activations[-1]
-        delta = np.zeros_like(out)
-
-        delta[action_index, 0] = out[action_index, 0] - out_target[action_index, 0]
-
-        dA = delta
+        dz = np.zeros_like(activations[-1])
+        dz[action_index, 0] = activations[-1][action_index, 0] - target_vec[action_index, 0]
+        dA = dz
 
         for i in reversed(range(len(self.weights))):
             a_prev = activations[i]
@@ -85,14 +91,14 @@ class Model_NN:
         
         # Update
         for i in range(len(self.weights)):
-            grad_w[i] = np.clip(grad_w[i], -max_grad_norm, max_grad_norm)
-            grad_b[i] = np.clip(grad_b[i], -max_grad_norm, max_grad_norm)
+            grad_w[i] = np.clip(grad_w[i], -100, 100)
+            grad_b[i] = np.clip(grad_b[i], -100, 100)
 
             self.weights[i] -= self.lr * grad_w[i]
             self.biases[i] -= self.lr * grad_b[i]
 
-            self.weights[i] = np.clip(self.weights[i], -1, 1)
-            self.biases[i] = np.clip(self.biases[i], -1, 1)
+            self.weights[i] = np.clip(self.weights[i], -100, 100)
+            self.biases[i] = np.clip(self.biases[i], -100, 100)
 
     # Save state, action, reward, action2 for replay
     def remember(self, s, a, r, s2, alive):
@@ -105,10 +111,9 @@ class Model_NN:
         batch = random.sample(self.replay, self.batch_size)
 
         for s, a, r, s2, alive in batch:
-            # Get prediction for the current state
             q_vals, activations, z_cache = self.forward_pass(s)
-            # Estimate value of the next state
-            next_q_vals, _, _ = self.forward_pass(s2)
+            # next_q_vals, _, _ = self.forward_pass(s2)
+            next_q_vals, _, _ = self.target_model.forward_pass(s2) # Important - use the target_model instead of the current one
             
             target_q = r if not alive else r + 0.99 * np.max(next_q_vals)
             target_vec = q_vals.copy()
@@ -118,12 +123,17 @@ class Model_NN:
     def action(self, game_state):
         state, alive = get_useful_state(game_state)
         out_val, _, _ = self.forward_pass(state)
-        print(out_val)
+        # if random.random() < 0.0005:
+        #     print(out_val)
         return int(np.argmax(out_val))
     
     def fit(self, episodes):
+        scores = []
+        epsilon = 0.9
         for episode in tqdm(range(episodes), colour='green'):
-            epsilon = max(0.001, 1.0 - episode / episodes)
+            score = 0
+            
+            epsilon = max(0.001, epsilon * 0.999)
             game = Game_engine()
             game_state = game.get_init_state()
             game_state = game.update(0)
@@ -137,12 +147,27 @@ class Model_NN:
 
                 next_game_state = game.update(action)
                 next_state, alive = get_useful_state(next_game_state)
-                reward = 1 if alive else -10
-                self.remember(state, action, reward, next_state, alive)
+                # reward = 1 if alive else -10
+                if not alive:
+                    reward = -10
+                else:
+                    x_position, game_info, end_info = game_state
+                    bird_height, bird_speed_vertical, next_pipe_low, next_pipe_high, next_pipe_distance = game_info
+                    next_pipe_mid = (next_pipe_low + next_pipe_high) / 2
 
+                    reward = 1 - abs(bird_height - next_pipe_mid) # Encourage staying close to the pipe gap
+
+                self.remember(state, action, reward, next_state, alive)
 
                 game_state = next_game_state
                 state = next_state
-            if episode % 5 == 0:
-                self.train()
+                score += reward
+            scores.append(score)
+            self.train()
+            
+            if episode % 50 == 0:
+                print(f"Avg reward (last 50 eps): {np.mean(scores[-50:])}")
+
+            if episode % 500 == 0:
+                self.update_target_model()
             
--- play_nn.py
+++ play_nn.py
@@ -9,7 +9,7 @@ from nn import *
 
 # Train model
 model = Model_NN()
-model.fit(10000)
+model.fit(20000)
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -31,8 +31,8 @@ while run:
     while run and not new_game:
         clock.tick(12)
 
-        jump = model.action(game_state)
-        print(jump)
+        jump = bool(model.action(game_state))
+        # print(jump)
 
         for e in pygame.event.get():
             if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):         # exit
